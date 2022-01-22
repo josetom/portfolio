1af8a6b23731665da342f3d2be44b39105148cc8
Jose Tom <josetom92@gmail.com>
2022-01-22 16:43:13 +0530

    fix: safelist to load dynamic tailwind css

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,13 +3,18 @@ const colors = require('tailwindcss/colors');
 module.exports = {
   content: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
   darkMode: 'class',
-  theme: {
-    extend: {
-      colors: {
-        teal: colors.teal,
-        cyan: colors.cyan,
-      },
+  // theme: {
+  //   extend: {
+  //     colors: {
+  //       teal: colors.teal,
+  //       cyan: colors.cyan,
+  //     },
+  //   },
+  // },
+  safelist: [
+    {
+      pattern: /grid-cols-.+/,
     },
-  },
+  ],
   plugins: [require('@tailwindcss/forms'), require('@tailwindcss/aspect-ratio')],
 };
