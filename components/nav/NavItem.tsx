c12678e68765db8b9c4e5ee7a6facf75070cd5f3
Jose Tom <josetom92@gmail.com>
2022-02-01 22:29:03 +0530

    chore: menu item css

--- components/nav/NavItem.tsx
+++ components/nav/NavItem.tsx
@@ -10,9 +10,9 @@ type NavItemProps = {
 export default function NavItem({ link, name, isSelected }: NavItemProps) {
   let className = classNames(
     isSelected
-      ? 'border-indigo-500 text-gray-900 dark:bg-gray-900 dark:text-white'
-      : 'border-transparent text-gray-500 hover:border-gray-300 hover:text-gray-700 dark:text-gray-300 dark:hover:bg-gray-700 dark:hover:text-white',
-    'inline-flex items-center px-1 pt-1 border-b-2 text-sm font-medium dark:px-3 dark:py-2 dark:rounded-md dark:border-none'
+      ? 'font-semibold text-gray-800 dark:text-gray-200 bg-gray-100 dark:bg-gray-900'
+      : 'font-normal text-gray-600 dark:text-gray-400',
+    'hidden md:inline-block p-1 sm:px-3 sm:py-2 rounded-lg hover:bg-gray-200 dark:hover:bg-gray-800 transition-all'
   );
 
   return (
